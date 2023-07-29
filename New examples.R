05e6471b7f3318866db4f1a3a1a266f61b0a1b1a
AdamCSmithCWS <adamcsmith.birdstats@gmail.com>
2023-07-29 07:05:17 -0400

    initial commit

---
+++ New examples.R
@@ -0,0 +1,23 @@
+
+
+# latlong example ---------------------------------------------------------
+
+
+
+
+# animated maps
+
+# Cross-validation
+
+
+# Covariate example (prairie climate)
+
+
+# model modification example (different prior or different derived parameter)
+
+
+
+
+
+
+
